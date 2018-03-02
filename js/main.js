635eeb9290494d0b1b83cefb5e6dfa01d4e41ff7
Andry Mahany <trysandy22@gmail.com>
2018-03-02 19:47:37 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.getElementById('output').style.visibility = 'hidden';
+
+document.getElementById('weightInput').addEventListener('input', function(e) {
+  let lbs = e.target.value;
+
+  if (lbs > 0) {
+    document.getElementById('output').style.visibility = 'visible';
+
+    document.getElementById('gramsOutput').innerHTML = lbs/0.0022046;
+    document.getElementById('kgOutput').innerHTML = lbs/2.2046;
+    document.getElementById('ozOutput').innerHTML = lbs*16;
+  } else {
+    document.getElementById('output').style.visibility = 'hidden';
+  }
+});
